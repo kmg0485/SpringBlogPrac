2416dec19b8379a7896408d4ab35b5fc9608146f
kmg0485 <kmg0485@naver.com>
2023-04-17 20:32:57 +0900

    Fix: url 통일화

--- src/main/java/com/sparta/spartablog/controller/BlogController.java
+++ src/main/java/com/sparta/spartablog/controller/BlogController.java
@@ -16,25 +16,25 @@ public class BlogController {
 
     private final BlogService blogService;
 
-    @GetMapping("/list") // http://localhost:8080/blog/list
+    @GetMapping("") // http://localhost:8080/blog
     public List<BlogResponseDto> getBlogList() {
         //테이블에 저장되어있는 모든 게시글 조회
         return blogService.getBlogList();
     }
 
-    @GetMapping("/list/{id}") // http://localhost:8080/blog/list/2
+    @GetMapping("/{id}") // http://localhost:8080/blog/2
     public BlogResponseDto getBlog(@PathVariable Long id) {
         //테이블에 저장되어있는 해당 게시글 조회
         return blogService.getBlog(id);
     }
 
-    @PostMapping("/create") // http://localhost:8080/blog/create
+    @PostMapping("") // http://localhost:8080/blog
     public Blog createBlog(@RequestBody BlogRequestDto requestDto) { //객체형식으로 넘어오기때문에 Body 사용
 
         return blogService.createBlog(requestDto);
     }
 
-    @PutMapping("/list/{id}") // http://localhost:8080/blog/list/2
+    @PutMapping("/{id}") // http://localhost:8080/blog/2
     public BlogResponseDto updateBlog(@PathVariable Long id, @RequestBody BlogRequestDto requestDto) {
         // 수정하기 위해 받아온 id를 사용하여 해당 게시글의 인스턴스가 존재하는지 확인하고 가져옴
             return blogService.updateBlog(id, requestDto);
